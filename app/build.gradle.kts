e57621bea572c5e2e7180c9c6fff55a077ee1481
MoacirBarreto <Moacirbarreto.ufrn@gmail.com>
2025-09-26 22:30:59 -0300

    Minha Media: 1commit 2 - acerto no layout e material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation ("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    
+
+
+
 }
